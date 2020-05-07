c8a337da15484f1979128bdee0fa8c65845dc82c
Medhanie <medhanie@outlook.com>
2020-05-06 21:57:20 -0700

    Initial commit.

---
+++ beilul-migration/src/test/java/io/medhanie/beilul/BeilulMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.medhanie.beilul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeilulMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
